28a45d5d58961bd313cf37898b56d793728467f7
N V N VAMSI KRISHNA <164296458+nani1418@users.noreply.github.com>
2025-09-25 21:52:00 +0530

    Update SpringBootServletInitializer.java

--- src/main/java/Forum/example/SpringBootServletInitializer.java
+++ src/main/java/Forum/example/SpringBootServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class SpringBootServletInitializer extends SpringBootServletInitializer {
-
+ 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ExampleApplication.class);
